41bb3a677988ada65ae3d46a87de85566fe8da1c
Nick Bautista <nbautista12@gmail.com>
2020-03-23 21:57:51 -0700

    Added better Validation to the Question Form

--- triviabros/forms.py
+++ triviabros/forms.py
@@ -1,7 +1,12 @@
+from triviabros.models import Question
 from flask_wtf import FlaskForm
 from flask_sqlalchemy import SQLAlchemy
-from wtforms import StringField, PasswordField, SubmitField, IntegerField
+from flask_login import current_user
+from wtforms import StringField, PasswordField, SubmitField, SelectField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+
+QUESTION_NUMBER_CHOICES = [('1', 1), ('2', 2), ('3', 3), ('4', 4), ('5', 5),
+                           ('6', 6), ('7', 7), ('8', 8), ('9', 9), ('10', 10)]
 
 
 class SignUpForm(FlaskForm):
@@ -21,14 +26,18 @@ class LoginForm(FlaskForm):
 
 
 class QuestionForm(FlaskForm):
-    question_number = IntegerField(
-        'Question Number', validators=[DataRequired()])
+    question_number = SelectField(
+        'Question Number', choices=QUESTION_NUMBER_CHOICES)
     question = StringField('Question', validators=[DataRequired()])
     submit = SubmitField('Add')
 
     def validate_question_number(self, question_number):
-        if not isinstance(question_number.data, int):
-            raise ValidationError('Must submit an Integer.')
+        print(question_number.data)
+        question = Question.query.filter_by(author=current_user).filter_by(
+            question_number=question_number.data).first()
+        if question:
+            raise ValidationError(
+                'This Question Number has already been used. Please choose the next number.')
 
 
 class AnswerForm(FlaskForm):
