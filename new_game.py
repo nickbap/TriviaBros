6054e7db9e0e0495aa500d6e1c1fbe7ea17e1840
Nick Bautista <nbautista12@gmail.com>
2020-03-25 21:01:41 -0700

    Added comments to new game script

--- new_game.py
+++ new_game.py
@@ -1,8 +1,14 @@
 from triviabros import db
 from triviabros.models import Question, Answer
 
+# This script will delete all records in the `questions` and `answers` table
+# so that a new game can be played. Please use with caution as it cannot be
+# undone once it has run!
 
-db.session.query(Question).delete()
-db.session.query(Answer).delete()
+question_num = db.session.query(Question).delete()
+answer_num = db.session.query(Answer).delete()
 
 db.session.commit()
+
+print("Successfully deleted {} questions!".format(question_num))
+print("Successfully deleted {} answers!".format(answer_num))
